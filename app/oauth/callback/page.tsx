50d9a9a40e785eb2f81f9276c434c6d2e52cb2c3
jgh진건희 <s23054@gsm.hs.kr>
2025-04-14 20:11:11 +0900

    feature: 로그인 페이지 개발

---
+++ app/oauth/callback/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+let tokenRequested = false; // 요청 중복 방지용 변수
+
+export default function OAuthCallbackPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+
+    if (!code) {
+      alert("code 없음");
+      router.push("/Signin");
+      return;
+    }
+
+    if (tokenRequested) return; // 중복 요청 방지
+    tokenRequested = true;
+
+    const fetchToken = async () => {
+      try {
+        const res = await axios.post("https://endlessly-cuddly-salmon.ngrok-free.app/auth/token", {
+          code,
+        });
+
+        localStorage.setItem("access_token", res.data.accessToken);
+        router.push("/");
+      } catch (err) {
+        console.error("로그인 실패:", err);
+        alert("소셜 로그인 실패");
+        router.push("/Signin");
+      }
+    };
+
+    fetchToken();
+  }, [router]);
+
+  return null;
+}
