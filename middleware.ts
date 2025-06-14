860fa53b55a3bd2be34842b1f9eb01505683b6a6
JinGeonhui <s23054@gsm.hs.kr>
2025-06-15 01:14:21 +0900

    feature: 미들웨어 설정

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const isExclude = 
+    pathname.startsWith("/Signin") ||
+    pathname.startsWith("/Signup") ||
+    pathname.startsWith("/_next") ||
+    pathname.startsWith('/icon/') ||
+    pathname.startsWith("/api") ||
+    pathname === "/favicon.ico";
+
+  if (isExclude) {
+    return NextResponse.next();
+  }
+
+  const token = request.cookies.get("access_token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/Signin", request.url));
+  }
+
+  return NextResponse.next();
+}
